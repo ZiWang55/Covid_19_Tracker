27b6c4d1706bec6595436bcfbd1c45f56fc4f02b
zi wang <ziwang55@gmail.com>
2021-03-06 13:56:41 -0600

    installing more dependencies

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
-const app = express();
-require("dotenv").config();
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const compression = require('compression')
+// const nodemailer = require('nodemailer')
+const routes = require('./routes')
+const PORT = process.env.PORT || 3001
+const app = express()
+require('dotenv').config()
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(compression())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 }
 
 // Define API routes here
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/covidtracker", {
+app.use(routes)
+// Connected to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/covidtracker', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
   useUnifiedTopology: true
-});
+})
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
-
+  console.log(`🌎 ==> API server now on port ${PORT}!`)
+})
