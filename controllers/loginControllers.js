47f60df935ef6f2b11337d1bea689f74f69a3eb0
James Leitschuh <james.leitschuh02@gmail.com>
2021-03-09 20:42:15 -0600

    added update funcion to controllers

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -12,13 +12,18 @@ module.exports = {
             .findById({ _id: rqe.params.id })
             .then(data => data.remove())
             .then(console.log("User Deleted Succesfully"))
-            .catch(err => res.status(422).json(err))
+            .catch(err => res.status(422).json(err));
     },
     findById: function(req,res) {
         db.Login
             .findById(req.params.id)
             .then(data => res.json(data))
-            .catch(err => res.status(422).json(err))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function(req,res) {
+        db.Login
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(data => res.json(data))
+            .catch(err => res.status(422).json(err));
     }
-
-}
+};
