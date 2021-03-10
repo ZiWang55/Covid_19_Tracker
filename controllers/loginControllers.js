4fbd0c75a31a0df0cc92dd675e209dd64768f23d
James Leitschuh <james.leitschuh02@gmail.com>
2021-03-09 20:36:00 -0600

    create and remove added to controller with functionality

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -1,5 +1,17 @@
 const db = require("../models");
 
 module.exports = {
-    db.login
+    create: function(req,res) {
+        db.Login
+            .create(req.body)
+            .then(data => res.json(data))
+            .error(err => res.status(422).json(err));
+    },
+    remove: function(req,res) {
+        db.Login
+            .findById({ _id: rqe.params.id })
+            .then(data => data.remove())
+            .then(console.log("User Deleted Succesfully"))
+            .catch(err => res.status(422).json(err))
+    }
 }
