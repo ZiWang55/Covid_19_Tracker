72242c5af98f16eb8acb992bdecf64fa029d70b0
Troy Dorman <troydorman@gmail.com>
2021-03-09 19:13:08 -0700

    set up database call to query all of the email addresses that are opted in

--- mailgun.js
+++ mailgun.js
@@ -1,14 +1,27 @@
 const mailgun = require('mailgun-js');
 require('dotenv').config()
+
+// this points to the database query model - may need to update later
+const db = require('./models/login.js')
+
+// you will need to add these to your local .env file
+// please see Slack chat for these two values
 const API_KEY = process.env.API_KEY
 const DOMAIN = process.env.DOMAIN
-const mg = mailgun({apiKey: process.env.API_KEY, domain: process.env.DOMAIN});
+
+// this variable will contain an array of email addresses queries from database
+const optInEmailList = [];
+
+const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
+
+// query database for all users that are opted-in for emails
+db.find({ opt_in : true }).then((emailList) => optInEmailList)
 
 
-console.log(API_KEY, DOMAIN)
+
 const emailData = {
 	from: 'Covid-19 Tracker <noreply@covid-track3r->',
-	to: 'troydorman@gmail.com',
+	to: optInEmailList,
 	subject: 'Hello',
 	text: 'Welcome to the Covid-19 Tracker'
 };
