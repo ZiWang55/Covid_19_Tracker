198e95dfc07f13efdea31582c0ed306baa3460d7
zi wang <ziwang55@gmail.com>
2021-03-11 21:44:00 -0600

    restyling login button, restyling new container

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -1,42 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import { fetchNews } from '../../api';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+// import Paper from '@material-ui/core/Paper';
+import { fetchNews } from "../../api";
+// import styles from './News.module.css';
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing('75%'),
-      height: theme.spacing('100%')
-    }
-  }
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//     '& > *': {
+//       margin: theme.spacing(1),
+//       width: theme.spacing(16),
+//       height: theme.spacing(16)
+//     }
+//   }
+// }));
 
 export default function News(props) {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   console.log(props.docs);
 
   return (
-    <div className={classes.root}>
-      <Paper>
-        <h3>Coronavirus News</h3>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="m">
+        <Typography
+          component="div"
+          style={{ color: 'grey', backgroundColor: "cream", height: "100%", padding: 10}}
+        >
+          <h3 style={{textAlign: 'center'}}>Coronavirus News</h3>
 
-        <ul>
-          {props.docs
-            ? props.docs.map((item, i) => (
-                <>
-                  <li key={i + 'li'}>
-                    <a href={item.web_url}>{item.abstract}</a>
-                  </li>
-                </>
-              ))
-            : ''}
-        </ul>
-      </Paper>
-    </div>
+          <ul>
+            {props.docs
+              ? props.docs.map((item, i) => (
+                  <>
+                    <li key={i + "li"}>
+                      <a href={item.web_url}>{item.abstract}</a>
+                    </li>
+                  </>
+                ))
+              : ""}
+          </ul>
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
