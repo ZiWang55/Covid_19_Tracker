7086b521ebc42a49d8932b59d5dd51ee48a85595
Peter Phenow <peter.phenow@gmail.com>
2021-03-11 22:06:48 -0600

    reworked news API to hide key

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -1,24 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import { fetchNews } from '../../api';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing('75%'),
-      height: theme.spacing('100%')
+      margin: theme.spacing(1)
+      // width: theme.spacing('75%'),
+      // height: theme.spacing('100%')
     }
   }
 }));
 
-export default function News(props) {
+function News(props) {
+  const [newsData, setNewsData] = useState();
+
+  const fetchNews = async () => {
+    try {
+      let dailyNews = await axios.get('/api/news');
+      // console.log('dailyNews', dailyNews.data.response);
+      return dailyNews.data.response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(async () => {
+    let gimmeMyData = await fetchNews();
+    setNewsData(gimmeMyData);
+    // console.log('gimmeMyData', gimmeMyData);
+  }, []);
+
   const classes = useStyles();
 
-  console.log(props.docs);
+  // console.log('newsData', newsData);
+  // console.log('newsData?.docs', newsData?.docs);
 
   return (
     <div className={classes.root}>
@@ -26,17 +45,19 @@ export default function News(props) {
         <h3>Coronavirus News</h3>
 
         <ul>
-          {props.docs
-            ? props.docs.map((item, i) => (
+          {newsData?.docs
+            ? newsData.docs.map((item, i) => (
                 <>
                   <li key={i + 'li'}>
                     <a href={item.web_url}>{item.abstract}</a>
                   </li>
                 </>
               ))
-            : ''}
+            : 'No data found'}
         </ul>
       </Paper>
     </div>
   );
 }
+
+export default News;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-const router = require("express").Router();
-const loginRoute = require("./login");
-const homepageRoute = require("./homepage");
-const settingsRoute = require("./settings")
+const router = require('express').Router();
+const loginRoute = require('./login');
+const homepageRoute = require('./homepage');
+const settingsRoute = require('./settings');
+import axios from 'axios';
+require('dotenv').config({ path: '../.env' });
 
 // Our routes here!
 
