9a8fb193690b2e49034b2cb225a549a374587c7d
Peter Phenow <peter.phenow@gmail.com>
2021-03-11 19:19:54 -0600

    created a news feed

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,55 @@
-import React from "react";
+import React from 'react';
 
-import { Cards, Chart, CountryPicker, Navbar } from "./components";
-import styles from "./App.module.css";
-import { fetchData } from "./api";
-import coronaImage from "./images/image.png"
+import { Cards, Chart, CountryPicker, Navbar, News } from './components';
+import styles from './App.module.css';
+import { fetchData, fetchNews } from './api';
+import coronaImage from './images/image.png';
 
 class App extends React.Component {
   state = {
     data: {},
-    country: "",
+    country: '',
+    news: ''
   };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    console.log(fetchedData);
-    this.setState({ data: fetchedData });
+    // console.log(fetchedData);
+    this.setState({ ...this.state, data: fetchedData });
+  }
+
+  //getting news data from API to display on News component
+  async componentDidMount() {
+    const fetchedNews = await fetchNews();
+    // console.log('fetchedNews', fetchedNews.data);
+
+    // set the news state to use as props
+    this.setState({ ...this.state, news: fetchedNews.data.response });
   }
 
   handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
-    console.log('fetcheddata',fetchedData);    
-  
+    // console.log('fetcheddata', fetchedData);
+
     // set the state
-    this.setState({ data: fetchedData, country: country });
+    this.setState({ ...this.state, data: fetchedData, country });
   };
 
   render() {
-    const { data, country } = this.state;
-    return (<div> <Navbar />
-      <div className={styles.container}>
-       
-        <img className={styles.image} alt="covid-19" src={coronaImage} />
-        
-        <Cards data={data} />
-        <hr />
-        <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Chart data={data} country={country} />
-      </div></div>
+    const { data, country, news } = this.state;
+    return (
+      <div>
+        <Navbar />
+        <div className={styles.container}>
+          <img className={styles.image} alt='covid-19' src={coronaImage} />
+
+          <Cards data={data} />
+          <hr />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} />
+          <News docs={news.docs} />
+        </div>
+      </div>
     );
   }
 }
--- client/src/components/CountryPicker/CountryPicker.jsx
+++ client/src/components/CountryPicker/CountryPicker.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { NativeSelect, FormControl } from "@material-ui/core";
-import styles from "./CountryPicker.module.css";
-import { fetchCountries } from "../../api";
+import React, { useEffect, useState } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api';
 
 const CountryPicker = ({ handleCountryChange }) => {
-
   const [fetchedCountries, setFetchedCountries] = useState([]);
-  
+
   useEffect(() => {
     const fetchAPI = async () => {
       setFetchedCountries(await fetchCountries());
@@ -15,15 +14,12 @@ const CountryPicker = ({ handleCountryChange }) => {
     fetchAPI();
   }, [setFetchedCountries]);
 
-  console.log(fetchedCountries);
+  // console.log(fetchedCountries);
 
   return (
     <FormControl className={styles.formControl}>
-      <NativeSelect
-        defaultValue=""
-        onChange={(e) => handleCountryChange(e.target.value)}
-      >
-        <option value="">Global</option>
+      <NativeSelect defaultValue='' onChange={(e) => handleCountryChange(e.target.value)}>
+        <option value=''>Global</option>
         {fetchedCountries.map((country, i) => (
           <option value={country} key={i}>
             {country}
