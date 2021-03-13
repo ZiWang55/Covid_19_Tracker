120560fb7d721cb6eb1da28af60a37ae20c67c1c
Troy Dorman <troydorman@gmail.com>
2021-03-13 14:41:20 -0700

    refactored the seeder file. only5 test users now, named after the group.  their email addreses are ours.  also changed the mailgun environment variables to be more explict

--- models/login.js
+++ models/login.js
@@ -6,50 +6,41 @@ const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 // Our login model
-const loginSchema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter your full name"
-    },
-    email: {
-      type: String,
-      match: /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/,
-      unique: true,
-      required: "Enter an email address"
-    },
-    password: {
-      type: String,
-      trim: true,
-      required: "Enter a password"
-    },
-    county: {
-      type: String,
-      trim: true,
-      required: "Enter your county name"
-    },
-    opt_in: {
-      type: Boolean
-    },
-    date: {
-      type: Date
-    }
-  }
-);
+const loginSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter your full name",
+  },
+  email: {
+    type: String,
+    match: /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/,
+    unique: true,
+    required: "Enter an email address",
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Enter a password",
+  },
+  county: {
+    type: String,
+    trim: true,
+    required: "Enter your county name",
+  },
+  opt_in: {
+    type: Boolean,
+  },
+});
 
 // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-User.prototype.validPassword = function(password) {
+User.prototype.validPassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 // Hooks are automatic methods that run during various phases of the User Model lifecycle
 // In this case, before a User is created, we will automatically hash their password
-User.addHook("beforeCreate", user => {
-  user.password = bcrypt.hashSync(
-    user.password,
-    bcrypt.genSaltSync(10),
-    null
-  );
+User.addHook("beforeCreate", (user) => {
+  user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
 });
 
 const Login = mongoose.model("Login", loginSchema);
