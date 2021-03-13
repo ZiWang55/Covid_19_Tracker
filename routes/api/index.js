041683a7ae1e01d07384f3f200ce5f92231c7346
Kai Dong <dongkai981@gmail.com>
2021-03-13 10:47:47 -0600

    Removed axios from routes index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const loginRoute = require('./login');
 const homepageRoute = require('./homepage');
 const settingsRoute = require('./settings');
-import axios from 'axios';
 require('dotenv').config({ path: '../.env' });
 
 // Our routes here!
