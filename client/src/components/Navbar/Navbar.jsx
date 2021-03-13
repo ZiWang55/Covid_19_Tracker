251323e72090d468eee45e7164566bbe082cd7c8
James Leitschuh <james.leitschuh02@gmail.com>
2021-03-13 10:34:32 -0600

    deleted imports

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Modal from '../Modal/Modal';
-import NewModal from "../NewModal/NewModal";
 
 const useStyles = makeStyles((theme) => ({
   root: {
