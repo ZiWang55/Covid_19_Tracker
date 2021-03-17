03da1e9a3fccce7dbb6cf7ff13ea3f22ec00a698
Kai Dong <dongkai981@gmail.com>
2021-03-16 20:07:13 -0500

    Updated router index to include logout

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const loginRoute = require('./login');
+const logoutRoute = require('./logout');
 const homepageRoute = require('./homepage');
 const settingsRoute = require('./settings');
 require('dotenv').config({ path: '../.env' });
 
 // Our routes here!
 router.use("/login", loginRoute);
+router.use("/logout", logoutRoute);
 router.use("/homepage", homepageRoute);
 router.use("/settings", settingsRoute);
 
