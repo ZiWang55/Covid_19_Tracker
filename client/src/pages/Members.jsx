f2df5b7a0b0217c021ab5459159db692d193347f
Kai Dong <dongkai981@gmail.com>
2021-03-19 08:37:41 -0500

    Removed unused imports in members page

--- client/src/pages/Members.jsx
+++ client/src/pages/Members.jsx
@@ -1,15 +1,6 @@
-import React, { useState } from "react";
-import API from "../api/Users";
-import {
-  TextField,
-  Button,
-  Typography,
-  ButtonGroup,
-  Container,
-  Grid,
-} from "@material-ui/core";
+import React from "react";
+import { Container, Grid } from "@material-ui/core";
 import Iframe from "../components/Iframe/Iframe";
-import News from "../components/News/News";
 
 function Members() {
 
