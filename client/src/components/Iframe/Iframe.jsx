1e8df210a813d2a54ccc54f89380cb3714a2fe19
Kai Dong <dongkai981@gmail.com>
2021-03-19 08:25:16 -0500

    Added more states to vacs1 graph

--- client/src/components/Iframe/Iframe.jsx
+++ client/src/components/Iframe/Iframe.jsx
@@ -22,16 +22,16 @@ const Iframe = ( source ) => {
           <iframe title="hospitalized" src="https://ourworldindata.org/grapher/current-covid-patients-hospital" className={styles.Iframe}></iframe>
         </Grid>
         <Grid item xs={6}>
-        <iframe title="confirmed" src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&time=40..latest&pickerSort=desc&pickerMetric=new_cases_per_million&Metric=Confirmed+cases&Interval=7-day+rolling+average&Relative+to+Population=true&Align+outbreaks=false&country=IND~USA~GBR~CAN~DEU~FRA&hideControls=true" className={styles.Iframe}></iframe>
+          <iframe title="confirmed" src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&time=40..latest&pickerSort=desc&pickerMetric=new_cases_per_million&Metric=Confirmed+cases&Interval=7-day+rolling+average&Relative+to+Population=true&Align+outbreaks=false&country=IND~USA~GBR~CAN~DEU~FRA&hideControls=true" className={styles.Iframe}></iframe>
         </Grid>
         <Grid item xs={6}>
-        <iframe title="deaths" src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&time=40..latest&pickerSort=desc&pickerMetric=new_deaths_per_million&Metric=Confirmed+deaths&Interval=7-day+rolling+average&Relative+to+Population=true&Align+outbreaks=false&country=IND~USA~GBR~CAN~DEU~FRA&hideControls=true" className={styles.Iframe}></iframe>
+          <iframe title="deaths" src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&time=40..latest&pickerSort=desc&pickerMetric=new_deaths_per_million&Metric=Confirmed+deaths&Interval=7-day+rolling+average&Relative+to+Population=true&Align+outbreaks=false&country=IND~USA~GBR~CAN~DEU~FRA&hideControls=true" className={styles.Iframe}></iframe>
         </Grid>
         <Grid item xs={6}>
-        <iframe title="vacs1" src='https://ourworldindata.org/grapher/us-daily-covid-vaccine-doses-administered?tab=chart&stackMode=absolute&time=earliest..latest&country=~USA&region=World' className={styles.Iframe}></iframe>
+          <iframe title="vacs1" src='https://ourworldindata.org/grapher/us-daily-covid-vaccine-doses-administered?country=Minnesota~California~Colorado~Hawaii~Illinois~GUM~Florida~Maine~New+York+State~Oregon~South+Carolina~Texas~Utah~Virginia~Wisconsin' className={styles.Iframe}></iframe>
         </Grid>
         <Grid item xs={12}>
-        <iframe title="vacs2" src='https://ourworldindata.org/grapher/us-total-covid-19-vaccine-doses-administered?tab=chart&stackMode=absolute&time=latest&country=Alaska~Arizona~California~Connecticut~Florida~GEO~Hawaii~Illinois~Indiana~Maine~Maryland~Massachusetts~Minnesota~Michigan~Missouri~New%20Jersey~New%20Mexico~New%20York%20State~North%20Carolina~North%20Dakota~Ohio~Oklahoma~Pennsylvania~South%20Dakota~Tennessee~Texas~Vermont~Virginia~Washington~West%20Virginia~Wisconsin&region=World' className={styles.Iframe}></iframe>
+          <iframe title="vacs2" src='https://ourworldindata.org/grapher/us-total-covid-19-vaccine-doses-administered?tab=chart&stackMode=absolute&time=latest&country=Alaska~Arizona~California~Connecticut~Florida~GEO~Hawaii~Illinois~Indiana~Maine~Maryland~Massachusetts~Minnesota~Michigan~Missouri~New%20Jersey~New%20Mexico~New%20York%20State~North%20Carolina~North%20Dakota~Ohio~Oklahoma~Pennsylvania~South%20Dakota~Tennessee~Texas~Vermont~Virginia~Washington~West%20Virginia~Wisconsin&region=World' className={styles.Iframe}></iframe>
         </Grid>
       </Grid>
     );
