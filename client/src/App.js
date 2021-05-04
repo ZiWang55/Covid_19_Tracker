4dd66aecdbebc349b2c28b31de50e4b0b7464b71
Troy Dorman <troydorman@gmail.com>
2021-05-04 17:25:48 -0600

    added press enter to search functionality and clearing the title value after doing so

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+.import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import NewUser from './pages/NewUser';
